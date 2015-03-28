236c48ad217c9091107b1b7a244f70bfd1d249c0
gazaway <j_i_m_m_y_g@hotmail.com>
2015-03-28 10:59:41 -0600

    Included mapper class to build on

---
+++ 480_a2/src/map/MapR.java
@@ -0,0 +1,139 @@
+package map;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class MapR {
+
+	public static class Map extends
+			Mapper<LongWritable, Text, Text, IntWritable> {
+		
+		private String date;
+		IntWritable right = new IntWritable();
+		Text left = new Text();
+		String cur = "";
+		String temp = "";
+		String hashedNum = "";
+		boolean haveDate = false;
+		boolean haveBookNum = false;
+		boolean haveStart = false;
+		
+
+		 public static boolean isInteger(String s) {
+             try { 
+                 Integer.parseInt(s); 
+             } catch(NumberFormatException e) { 
+                 return false; 
+             }
+             return true;
+         }
+		
+		@Override
+		public void map(LongWritable key, Text inputBook, Context context)
+				throws IOException, InterruptedException {
+
+			String bookStrings = inputBook.toString();
+			StringTokenizer lineToke = new StringTokenizer(bookStrings, "\r\n");
+			while (lineToke.hasMoreTokens() && (!haveDate || !haveBookNum)) {
+				cur = lineToke.nextToken();
+				cur = cur.replaceAll("\\p{P}", "");
+				if (cur.contains("EBook")) {
+					StringTokenizer bookNumToke = new StringTokenizer(cur);
+					while (bookNumToke.hasMoreTokens()) {
+						temp = bookNumToke.nextToken();
+						if (temp.contains("EBook")){
+							temp = bookNumToke.nextToken();
+							if (isInteger(temp)){
+								hashedNum = temp;
+								haveBookNum = true;
+							}
+						}
+					}
+				} 
+				if (cur.contains("Release Date")){
+					StringTokenizer dateToke = new StringTokenizer(cur);
+					while (dateToke.hasMoreTokens() && !haveDate){
+						temp = dateToke.nextToken();
+						if ((temp.length() == 4) && (isInteger(temp)) && ((temp.charAt(0) == '1') || (temp.charAt(0) == '2'))) {
+							date = temp;
+							haveDate = true;
+						}
+					}
+				}
+			}
+			while (lineToke.hasMoreTokens()){
+				cur = lineToke.nextToken();
+				cur = cur.replaceAll("\\p{P}", "");
+				StringTokenizer bookToke = new StringTokenizer(cur);
+				while (bookToke.hasMoreTokens()){
+					temp = bookToke.nextToken();
+					temp = temp.toLowerCase();
+					if (temp != "") {
+						left.set(temp + '\t' + date + '\t');
+						if (isInteger(hashedNum)) {
+							int temp = Integer.parseInt(hashedNum);
+							right.set(temp);
+						} else {
+							right.set(0);
+						}
+						context.write(left, right);
+					}
+				}
+			}
+		}
+	}
+
+	public static class Reduce extends
+			Reducer<Text, IntWritable, Text, IntWritable> {
+
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			Set<IntWritable> bookSet = new HashSet<IntWritable>();
+			for (IntWritable val : values) {
+				bookSet.add(val);
+				sum++;
+			}
+			String leftS = key.toString() + '\t' + sum + '\t';
+			int rightValue = bookSet.size();
+			context.write(new Text(leftS), new IntWritable(rightValue));
+		}
+	}
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+
+		Job job = new Job(conf, "wordcount");
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		job.setMapperClass(Map.class);
+		job.setReducerClass(Reduce.class);
+
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		job.waitForCompletion(true);
+	}
+
+}
