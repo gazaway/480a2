1a14af4e1a372886dfc07fc59a3792452b5a27ff
Jim <Jim@129.82.98.57>
2015-03-31 17:15:24 -0600

    Third running might be done :) Separated calculations into their own methods. TODO: formating/encrpytion.

--- 480_a2/src/map/ThirdRunning.java
+++ 480_a2/src/map/ThirdRunning.java
@@ -1,16 +1,15 @@
 package map;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.HashMap;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.DF;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Mapper.Context;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
@@ -53,20 +52,19 @@ public class ThirdRunning {
 	        for (String document : temp.keySet()) {
 	            String[] numSplit = temp.get(document).split("/");
 	 
-	            //Term frequency is the quocient of the number of terms in document and the total number of terms in doc
-	            double tf = Double.valueOf(Double.valueOf(numSplit[0])
-	                    / Double.valueOf(numSplit[1]));
+	            //Get the TF
+	            double tf = getTF(numSplit[0], numSplit[1]);	
+	            //Get the idf
+	            double idf = getIDF(numOfDocs, numOfDocsWithWord);		
+	            //tf.idf = tf * idf. Format and store.
+	            DecimalFormat formatter = new DecimalFormat("#0.000");
+	            double tfIdf = tf * idf;
+	            String tfIdfString = formatter.format(tfIdf);
 	 
-	            //interse document frequency quocient between the number of docs in corpus and number of docs the term appears
-	            double idf = (double) numOfDocs / (double) numOfDocsWithWord;
-	 
-	            //given that log(10) = 0, just consider the term frequency in documents
-	            double tfIdf = numOfDocs == numOfDocsWithWord ?
-	                    tf : tf * Math.log10(idf);
-	 
-	            context.write(new Text(key + "@" + document), new Text("[" + numOfDocsWithWord + "/"
-	                    + numOfDocs + " , " + numSplit[0] + "/"
-	                    + numSplit[1] + " , " + DF.format(tfIdf) + "]"));
+	            //write the context. Offline portion is done :) YAY
+	            //TODO NEED TO SETUP OUTPUT TO GIVE DOCUMENT, KEY, IDF FORMAT. You already have everything.
+	            //TODO Some sort of encryption :) Have fun with it!
+	            context.write(new Text(key + "@" + document), new Text(tfIdfString));
 	        }
 	    }
 	}
@@ -95,6 +93,31 @@ public class ThirdRunning {
 		FileOutputFormat.setOutputPath(job, new Path(arg));
 
 		job.waitForCompletion(true);
+	}
+
+	/*
+	 * Returns the IDF of the given ints. IDF = log2(N/ni). N = # of docs.
+	 * ni = # of docs with the word. If # of docs == # docs with the word
+	 * (AKA every doc has the word, I decided to return 1.
+	 */
+	public double getIDF(int _numOfDocs, int _numOfDocsWithWord) {
+		if (_numOfDocs == _numOfDocsWithWord){
+			return (double)_numOfDocs / (double)_numOfDocsWithWord;
+		}
+		else {
+			DecimalFormat formatter = new DecimalFormat("#0.000");
+			double temp = (double)((Math.log((double)_numOfDocs / (double)_numOfDocsWithWord)) / Math.log(2));
+            String idfString = formatter.format(temp);
+			return Double.parseDouble(idfString);
+		}
+	}
+
+	/*
+	 * Returns the TF of the given strings. TF = f/MAX(f). f = freq of word in document.
+	 * MAX(f) = max # of occurences in any document.
+	 */
+	public double getTF(String string, String string2) {
+		return (Double.valueOf(Double.valueOf(string) / Double.valueOf(string2)));
 	}
 
 	public boolean setNumDocs(int num_Docs) {
